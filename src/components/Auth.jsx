04723c202ef3ed9c3624957ce8b818e0b2b3ab02
BlazeSN860 <virajprabhup@gmail.com>
2024-02-02 23:22:15 +0530

    second commits

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { auth, provider, emailProvider } from "../firebase-config"
 import { signInWithPopup } from "firebase/auth"
+import '../styles/Auth.css'
 
 import Cookies from 'universal-cookie'
 const cookies = new Cookies()
@@ -28,7 +29,17 @@ const Auth = (props) => {
     }
 
     const signUpWithEmail = async () => {
-
+        // createUserWithEmailAndPassword(auth, email, password)
+        //     .then((userCredential) => {
+        //         // Signed up 
+        //         const user = userCredential.user;
+        //         // ...
+        //     })
+        //     .catch((error) => {
+        //         const errorCode = error.code;
+        //         const errorMessage = error.message;
+        //         // ..
+        //     });
     }
 
     const changeOption = (value) => {
@@ -39,20 +50,31 @@ const Auth = (props) => {
     return (
         <div className="auth">
             <h1>Chatter</h1>
-            <button id="option" onClick={() => changeOption(false)}>Sign Up</button>
-            <button id="option" onClick={() => changeOption(true)}>Sign In</button>
+            <div className='auth-container'>
+                <div className='auth-options'>
+                    <button id="option" autoFocus onClick={() => changeOption(false)} disabled>Sign Up</button>
+                    <button id="option" onClick={() => changeOption(true)} disabled>Sign In</button>
+                </div>
 
-            {signIn ? (
-                <div>
-                    <p>Sign In with Email</p>
-                    <button onClick={signInWithEmail}>Sign In with Email</button>
+                <div className='emailpass'>
+                    <div>
+                        <label htmlFor="email1">Email: </label>
+                        <input type="text" id='email1' disabled />
+                    </div>
+                    <div>
+                        <label htmlFor="password1">Password: </label>
+                        <input type="password" id='password1' disabled />
+                    </div>
+                    {signIn ? (
+                        <button onClick={signInWithEmail} disabled>Sign In with Email</button>
+                    ) : (
+
+                        <button onClick={signUpWithEmail} disabled>Sign Up with Email</button>
+                    )}
+                    <h1>Feature Coming Soon</h1>
                 </div>
-            ) : (
-                <div>
-                    <p>Sign Up with Email</p>
-                    <button onClick={signUpWithEmail}>Sign Up with Email</button>
-                </div>
-            )}
+
+            </div>
             <h4>OR</h4>
             <p>Sign In with Google</p>
             <button onClick={signInWithGoogle}>Sign In with Google</button>
